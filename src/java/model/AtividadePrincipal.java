d39f8b295c38376acf8f7961036f20670edfcb88
Matheus Galbero Zanetoni <matheus.zanetoni@etec.sp.gov.br>
2022-02-24 10:58:34 -0300

    Criação dos pacotes Utils e Model e das classes de conexão, Pessoa, Paciente, Medico e Atividade Principal

---
+++ src/java/model/AtividadePrincipal.java
@@ -0,0 +1,28 @@
+package model;
+
+public class AtividadePrincipal {
+
+    private int codigoAtividadePrincipal;
+    private String descricaoAtividadePrincipal;
+
+    public AtividadePrincipal(int codigoAtividadePrincipal, String descricaoAtividadePrincipal) {
+        this.codigoAtividadePrincipal = codigoAtividadePrincipal;
+        this.descricaoAtividadePrincipal = descricaoAtividadePrincipal;
+    }
+
+    public int getCodigoAtividadePrincipal() {
+        return codigoAtividadePrincipal;
+    }
+
+    public void setCodigoAtividadePrincipal(int codigoAtividadePrincipal) {
+        this.codigoAtividadePrincipal = codigoAtividadePrincipal;
+    }
+
+    public String getDescricaoAtividadePrincipal() {
+        return descricaoAtividadePrincipal;
+    }
+
+    public void setDescricaoAtividadePrincipal(String descricaoAtividadePrincipal) {
+        this.descricaoAtividadePrincipal = descricaoAtividadePrincipal;
+    }
+}
---
+++ src/java/model/Medico.java
@@ -0,0 +1,29 @@
+package model;
+
+public class Medico extends Pessoa {
+
+    private String crmMedico;
+    private boolean statusMedico;
+
+    public Medico(String crmMedico, boolean statusMedico, int codigoPessoa, String nomePessoa, String dataNascimentoPessoa, String cpfPessoa, String senhaPessoa) {
+        super(codigoPessoa, nomePessoa, dataNascimentoPessoa, cpfPessoa, senhaPessoa);
+        this.crmMedico = crmMedico;
+        this.statusMedico = statusMedico;
+    }
+
+    public String getCrmMedico() {
+        return crmMedico;
+    }
+
+    public void setCrmMedico(String crmMedico) {
+        this.crmMedico = crmMedico;
+    }
+
+    public boolean isStatusMedico() {
+        return statusMedico;
+    }
+
+    public void setStatusMedico(boolean statusMedico) {
+        this.statusMedico = statusMedico;
+    }
+}
