0568c6d4b8fce2e6e3507e4e0d7980278bad2924
Matheus Galbero Zanetoni <matheus.zanetoni@etec.sp.gov.br>
2022-04-28 11:06:49 -0300

    Criação do banco e interfaces gráficas de atividade principal

--- src/java/controller/CadastrarAtividadePrincipal.java
+++ src/java/controller/CadastrarAtividadePrincipal.java
@@ -16,6 +16,7 @@ public class CadastrarAtividadePrincipal extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
+        request.setCharacterEncoding("UTF-8");
         try {
             int codigoAtividadePrincipal = request.getParameter("codigoAtividadePrincipal").isEmpty() ? 0 : Integer.parseInt(request.getParameter("codigoAtividadePrincipal"));
             
@@ -31,7 +32,7 @@ public class CadastrarAtividadePrincipal extends HttpServlet {
         } catch (SQLException | ClassNotFoundException ex) {
             request.setAttribute("mensagem", ex.getMessage());
         }
-        request.getRequestDispatcher("NovaAtividadePrincipal").forward(request, response);
+        request.getRequestDispatcher("ListarAtividadePrincipal").forward(request, response);
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
